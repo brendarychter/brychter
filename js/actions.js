2eea4d4a4ae5277557c1d284a790fe7e33e0b707
Brenda Rychter <brendarychter@gmail.com>
2016-12-17 21:35:45 -0300

    first image

--- js/actions.js
+++ js/actions.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
-	console.log('hola')
 
+	//Scroll with animation home
 	$('.what-i-do').on('click', function(){
-		//$("html, body").delay(6000).animate({scrollTop: $('#main-section').offset().top }, 2000);
 		$('html, body').animate({
-        scrollTop: $("#main-section").offset().top
-    }, 1000);
+	        scrollTop: $("#main-section").offset().top
+	    }, 1000);
 	})
+
+	//Array fotos para slider de home
+	var arrayPhotos = ["img1.png", "img2.png", "video1.mp4", "img3.png", "video2.mp4"]
 });
