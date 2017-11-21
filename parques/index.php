924435a9f89d254feb2ee12661c709a8dfe9758b
Brenda Rychter <brendarychter@gmail.com>
2017-11-20 23:42:11 -0300

    parques

---
+++ parques/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE <html>
+<head>
+	<meta charset="UTF-8">
+	<title>Continuidad de los parques</title>
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+
+	<!-- Librerias externas -->
+    <script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+    <script type="text/javascript" src="js/svg.min.js"></script>
+
+    <!-- My js files -->
+    <script type="text/javascript" src="js/actions.js"></script>
+</head>
+<body>
+	<div class="container">
+		<div class="content">
+			<div id="drawing"></div>
+		</div>
+	</div>
+</body>
+</html>
